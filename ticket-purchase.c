70f8241104ff8298efcbcfa63ff27fb15df9fc0a
poiwert <max080405@ukr.net>
2023-07-13 10:53:17 +0300

    added ticket-purchase.c

---
+++ ticket-purchase.c
@@ -0,0 +1,9 @@
+int buy_tickets(){
+    int input_key;
+    printf("Click '1' to buy ticket");
+    scanf("%d",&input_key);
+    if(input_key==1){
+        printf("purchase complete");
+    }
+    else printf("wrong number");
+}
