f114bf9bfb2056ee433e10d862584711250a28ab
Nastya <138471960+Kharchenko-Anastasia@users.noreply.github.com>
2023-07-13 19:35:04 +0300

    added file main_menu

---
+++ main_menu.c
@@ -0,0 +1,23 @@
+int main() {
+    int menu_section, info, buy;
+    while (1) {
+        printf("Choose menu section\n");
+        printf("__________________________________\n");
+        printf("\t Account(1)\t Buy(2)\n");
+        scanf("%d", &menu_section);
+        printf("__________________________________\n");
+        
+        switch (menu_section) {
+            case 1:
+                info = account_info();
+                break;
+            case 2:
+                buy = buy_tickets();
+                break;
+            default:
+                printf("Input another key\n");
+                break;
+        }
+    }
+    return 0;
+}
